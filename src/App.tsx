69e0b21b9f1b01779763d8741761f81ce7445dd6
Akinmyrza Kanat uulu <akinmyrza2000@gmail.com>
2021-06-24 15:10:45 +0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ActionCreators, State } from './state';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney, bankruptMoney } = bindActionCreators(
+    ActionCreators,
+    dispatch,
+  );
+
+  const amount = useSelector((state: State) => state.bank);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{amount}</h1>
+      <button onClick={() => depositMoney(1000)}>DEPOSIT</button>
+      <button onClick={() => withdrawMoney(500)}>WITHDRAW</button>
+      <button onClick={() => bankruptMoney()}>BANKRUPT</button>
     </div>
   );
 }
---
+++ src/state/actions/action.ts
@@ -0,0 +1,15 @@
+import { ActionType } from '../action-types/index';
+
+interface DepositeAction {
+  type: ActionType.DEPOSIT;
+  payload: number;
+}
+interface WithdrawAction {
+  type: ActionType.WITHDRAW;
+  payload: number;
+}
+interface BankruptAction {
+  type: ActionType.BANKRUPT;
+}
+
+export type Action = DepositeAction | WithdrawAction | BankruptAction;
